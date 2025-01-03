46e663ddc515a2821213cdf1ee06a63a03282b2a
olesyamaniuk <maniukolesya222@gmail.com>
2025-01-03 15:02:39 +0200

    1

---
+++ js/mobile-menu.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const navListRef = document.querySelector('.navigation__list');
+  const headerRef = document.querySelector('.header');
+  const navigationRef = document.querySelector('.navigation'); 
+
+  menuBtnRef.addEventListener('click', () => {
+    menuBtnRef.classList.toggle('is-open');
+    navListRef.classList.toggle('is-open');
+    headerRef.classList.toggle('is-open');
+    navigationRef.classList.toggle('is-open'); 
+  });
+
+  document.querySelectorAll('.navigation__link').forEach((n) =>
+    n.addEventListener('click', () => {
+      navListRef.classList.remove('is-open');
+      menuBtnRef.classList.remove('is-open');
+      headerRef.classList.remove('is-open');
+      navigationRef.classList.remove('is-open'); 
+    })
+  );
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/slick.js
@@ -0,0 +1,71 @@
+$(function () {
+  $('.gallery__list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.gallery__list-left',
+    nextArrow: '.gallery__list-right',
+  });
+});
+
+
+$(function () {
+  $('.features-list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.features__list-left',
+    nextArrow: '.features__list-right',
+  });
+});
+$(function () {
+  $('.advantages-list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.advantages__list-left',
+    nextArrow: '.advantages__list-right',
+  });
+});
+
+$(function () {
+  $('.gallery-diary__list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.gallery-diary__list-left',
+    nextArrow: '.gallery-diary__list-right',
+  });
+});
+
+
+
+
